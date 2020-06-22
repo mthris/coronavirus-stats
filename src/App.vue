7649b302b4fa3bbdea4193890032a89c714389a6
mthris <xmthris@gmail.com>
2020-06-22 19:54:30 +0200

    create current stats

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,11 @@
     <div class="container">
       <img class="sars" src="./assets/sars.png" alt="sars"/>
       <Header/>
+      <div class="cases-wrapper">
+        <p class="cases confirmed">zakażonych <span class="cases__confirmed">{{ confirmed() }}<span class="cases-num">+{{ differenceConfirmed() }}</span></span></p>
+        <p class="cases deaths">zmarłych <span class="cases__deaths">{{ deaths() }}<span class="cases-num">+{{ differenceDeaths() }}</span></span></p>
+        <p class="cases recovered">wyleczonych <span class="cases__recovered">{{ recovered() }}<span class="cases-num">+{{ differenceRecovered() }}</span></span></p>
+      </div>
     </div>
   </div>
 </template>
@@ -12,8 +17,44 @@ import Header from './components/Header.vue';
 
 export default {
   name: 'App',
+  data() {
+    return {
+      cases: null
+    }
+  },
+  methods: {
+    confirmed(){
+      return this.cases.filter((v, i, arr) => i === arr.length - 1)[0].Confirmed.toLocaleString()
+    },
+    deaths(){
+      return this.cases.filter((v, i, arr) => i === arr.length - 1)[0].Deaths.toLocaleString()
+    },
+    recovered(){
+      return this.cases.filter((v, i, arr) => i === arr.length - 1)[0].Recovered.toLocaleString()
+    },
+    differenceConfirmed(){
+      const last = this.cases.filter((v, i, arr) => i === arr.length - 1)[0].Confirmed;
+      const beforeLast = this.cases.filter((v, i, arr) => i === arr.length - 2)[0].Confirmed;
+      return last - beforeLast;
+    },
+    differenceDeaths(){
+      const last = this.cases.filter((v, i, arr) => i === arr.length - 1)[0].Deaths;
+      const beforeLast = this.cases.filter((v, i, arr) => i === arr.length - 2)[0].Deaths;
+      return last - beforeLast;
+    },
+    differenceRecovered(){
+      const last = this.cases.filter((v, i, arr) => i === arr.length - 1)[0].Recovered;
+      const beforeLast = this.cases.filter((v, i, arr) => i === arr.length - 2)[0].Recovered;
+      return last - beforeLast;
+    }
+  },
   components: {
     Header
+  },
+  async created(){
+    const promise = await fetch('https://api.covid19api.com/total/dayone/country/poland');
+    const cases = await promise.json();
+    return this.cases = cases;
   }
 }
 </script>
@@ -32,6 +73,26 @@ overflow-x: hidden;
 html, body {
 height:100%;
 width:100%;
+}
+
+.cases {
+position: relative;
+}
+
+.cases-num{
+position: absolute;
+font-size: 18px;
+color: #0ee76f;
+right: -14px;
+top: -27px;
+}
+
+.cases-wrapper{
+display: flex;
+flex-direction: row;
+justify-content: space-between;
+font-size: 38px;
+margin-top: 143px;
 }
 
 .container{
