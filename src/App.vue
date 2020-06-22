f2b345f3cad036aba743e5af7f6bc9d526b59693
mthris <xmthris@gmail.com>
2020-06-22 15:44:54 +0200

    create header component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,41 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import Header from './components/Header.vue';
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header
   }
 }
 </script>
 
 <style>
+@import url('https://fonts.googleapis.com/css2?family=Roboto:wght@900&display=swap');
+*{
+margin: 0;
+padding: 0;
+}
+
+html, body {
+height:100%;
+width:100%;
+}
+
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  background: #db1637;
+  color: #ffffff;
+  font-family: 'Roboto', sans-serif;
+
 }
 </style>
---
+++ src/components/Header.vue
@@ -0,0 +1,25 @@
+<template>
+  <header class="header">
+    <h2 class="header__title">Koronawirus w Polsce</h2>
+  </header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+
+</script>
+
+<style>
+  .header {
+  max-width: 400px;
+  margin: 0 auto;
+  padding-top: 153px;
+  }
+
+  .header__title{
+  font-size: 65px;
+  text-align: center;
+  }
+</style>
